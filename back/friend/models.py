10b4a1962cf8f81ac8dc18615a26ad182264c69b
nakazkan <68913064+nakazkan@users.noreply.github.com>
2022-11-22 00:44:44 +0300

    Update models.py

--- back/friend/models.py
+++ back/friend/models.py
@@ -1,9 +1,11 @@
 from django.db import models 
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
+from customer.models import Customer
 
-class Friend(models.Model):
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
-    friendId = models.BigIntegerField(default=0)
 
+class Friend(models.Model): #following
+    userId = models.ForeignKey(Customer, related_name='userId', on_delete=models.CASCADE)
+    friendId = models.ForeignKey(Customer, related_name='friendId', on_delete=models.CASCADE)
+    
     def __str__(self) -> str:
-        return self.userId
+        return 'friend'
