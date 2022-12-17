893510cc095e4909fe3f04540143c559a6332487
nakazkan <68913064+nakazkan@users.noreply.github.com>
2022-12-17 19:20:33 +0300

    Update serializers.py

--- back/customer/serializers.py
+++ back/customer/serializers.py
@@ -9,20 +9,9 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'first_name',
                   'last_name', 'email', 'password']
-        # write_only_fields = ('password',)
-    #    read_only_fields = ('id',)
         extra_kwargs = {
             'password': {'write_only': True}
         }
-        # def create(self, validated_data):
-        #     user = User.objects.create(
-        #     username=validated_data['username'],
-        #     email=validated_data['email'])
-
-        #     user.set_password(validated_data['password'])
-        #     user.save()
-
-        #     return user
 
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -32,13 +21,6 @@ class CustomerSerializer(serializers.ModelSerializer):
         model = Customer
         fields = ['id', 'image', 'user']
         authentication_classes = [TokenAuthentication]
-        #extra_kwargs = {'id': {'required': False}}
-        # extra_kwargs = {
-        #     'user': {'write_only': True}
-        # }
-        # fields = ['id','email',
-        #           'password', 'name', 'lastname', 'image']
-        #extra_kwargs = {'image': {'required': False}}
 
     def create(self, validated_data):
         user_data = validated_data.pop('user')
@@ -52,12 +34,3 @@ class CustomerSerializer(serializers.ModelSerializer):
         instance = super().update(instance, user_data)
         Customer.objects.filter(user=instance).update(**validated_data)
         return instance
-
-    # def create(self, validated_data):
-
-    #     user_data = validated_data.pop('user')
-    #     user = UserSerializer.create(
-    #         UserSerializer(), validated_data=user_data)
-    #     customer, created = Customer.objects.update_or_create(
-    #         id=validated_data.pop('id'), name=validated_data.pop('name'), lastname=validated_data.pop('lastname'), image=validated_data.pop('image'), user=user,)
-    #     return customer
